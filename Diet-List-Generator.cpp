f0b645ba7aa624d5c9ee4fdb3daba23c2680475e
Mehmet Emin Uzunyayla <59115767+mehmetuzunyayla@users.noreply.github.com>
2023-11-29 23:17:37 +0300

    Diet-List-Generator.cpp

--- Diet-List-Generator.cpp
+++ Diet-List-Generator.cpp
@@ -6,7 +6,7 @@ int main() {
     string name;
     float weight, height, exercise_hours;
 
-    // Using "getInfo()" function to get our client's information.
+    // Using the "getInfo()" function to get our client's information.
     getInfo(name, weight, height, exercise_hours);
 
     // Calculating BMI using the "calculateBMI()" function and assigning the result to a variable for later use.
